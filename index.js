2f30aed7c8cf67968cd8fbe95c5bd2661c887a32
yuanshengpan <1441433972@qq.com>
2020-08-31 08:51:32 +0800

    aa

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-$(function() {
-    $('.navbar-nav li').click(function() {
-        console.log("123");
+$(function () {
+    //点击高亮
+    $(".navbar-nav li").on("click", function () {
+        // $(this).addClass('active').siblings().removeClass('active');
+        $("html").animate({
+            scrollTop: $(".clientHeight").eq($(this).index()).offset().top
+        })
+        // return false
+    });
+    //高度自适应
+    $(document).ready(function () {
+        //初始化、高度  
+        $(".clientHeight").height($(window).height());
+        $(".nameHeight").css({ "height": "calc(100% - 56px)" });
+        //当文档窗口发生改变时 触发  
+        $(window).resize(function () {
+            $(".clientHeight").height($(window).height());
+            $(".header").css({ "background-size": "100% 100%" })
+        })
     })
+    //滑动高亮
+    $(window).scroll(function () {
+        if ($(window).scrollTop() == 0) {
+            $("nav").removeClass('bg-light');
+        } else {
+            $("nav").addClass('bg-light');
+        }
+        for (let i = 0; i < $(".clientHeight").length; i++) {
+            if ($(window).scrollTop() >= $(".clientHeight").eq(i).offset().top) {
+                $('.navbar-nav li').eq(i).addClass('active').siblings().removeClass('active');
+            }
+        }
+    });
 })
